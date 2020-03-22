f175862ea8eddf2d1f0c7c0e1b128c17ca51b075
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2020-03-22 17:05:04 +0200

    feat: add solution for basic test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,76 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const operators = {
+        "+": {
+            priority: 1,
+            action: (a, b) => a + b
+        },
+        "-": {
+            priority: 1,
+            action: (a, b) => a - b
+        },
+        "*": {
+            priority: 2,
+            action: (a, b) => a * b
+        },
+        "/": {
+            priority: 2,
+            action: (a, b) => a / b
+        },
+        "(": {},
+        ")": {}
+    };
+
+    let stackNumber = [];
+    let stackOperators = [];
+    let arr = expr.trim().split(' ').filter(e => e !== ' ').filter(e => e !== '');;
+
+    if (arr.join('').includes("/0")) {
+        throw new Error("TypeError: Division by zero.");
+    }
+
+    if (arr.join('').replace(/[^(]/g, "").length != arr.join('').replace(/[^)]/g, "").length) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let current = arr[i];
+
+        if (Number(current)) {
+            stackNumber.push(parseFloat(current));
+        };
+        if (current in operators) {
+            if (stackOperators.length === 0) {
+
+                stackOperators.push(current);
+
+            } else {
+
+                let top = stackOperators[stackOperators.length - 1];
+
+                if (operators[top].priority < operators[current].priority) {
+                    stackOperators.push(current);
+                } else {
+                    let b = stackNumber.pop();
+                    let a = stackNumber.pop();
+
+                    stackNumber.push(operators[top].action(a, b));
+                    stackOperators.pop();
+                    i--
+                };
+            };
+        };
+    };
+
+    for (let j = 0; j < stackOperators.length; j++) {
+        let top = stackOperators[stackOperators.length - 1];
+        let b = stackNumber.pop();
+        let a = stackNumber.pop();
+        stackNumber.push(operators[top].action(a, b));
+        stackOperators.pop();
+    }
+    return Number(stackNumber);
 }
 
 module.exports = {
